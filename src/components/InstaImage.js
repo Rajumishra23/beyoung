f1f3abcf1a6cc6fce155ebb3e1009548ac15dda9
unknown <your-email@example.com>
2025-09-07 02:23:57 +0530

    Updated layout for better responsive view

--- src/components/InstaImage.js
+++ src/components/InstaImage.js
@@ -66,10 +66,10 @@ const InstaImage = () => {
       className="relative w-56 sm:w-56 md:w-64 flex-shrink-0 rounded-xl overflow-hidden group cursor-pointer 
       border border-gray-200 shadow-md hover:shadow-xl transition"
     >
-     <img
+    <img
   src={item.image}
   alt={item.title}
-  className="w-[90%] max-w-[500px] h-[250px] sm:w-full sm:h-64 md:max-w-[600px] md:h-80 lg:max-w-[700px] lg:h-[450px] object-cover transition-transform duration-300 group-hover:scale-110 mx-auto"
+  className="w-[100%] max-w-[500px] h-[300px] sm:w-full sm:h-64 md:max-w-[600px] md:h-80 lg:max-w-[700px] lg:h-[450px] object-cover transition-transform duration-300 group-hover:scale-110 mx-auto"
 />
 
 
